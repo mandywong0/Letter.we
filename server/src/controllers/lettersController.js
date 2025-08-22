56b758e0d6d992e16e8e2f214803075b17bd6a1d
mandywong00 <mandywong@berkeley.edu>
2025-08-22 15:47:12 -0700

    finished mailLetter function v1

--- server/src/controllers/lettersController.js
+++ server/src/controllers/lettersController.js
@@ -1,23 +1,31 @@
+const Letter = require('../models/Letter');
+
 //for POST letter
 const mailLetter = async (req, res) => {
   try {
 
-    let { content } = req.body;
+    let { content } = req.body.trim();
     const senderID = req.user.id;
     const recipientID = 1; //change to paired partner later
 
-    //validate 
+    //validate user input
+    if (!content || typeof content !== "string" || !content.trim()) {
+      return res.status(400).json({ error: "Cannot send a blank letter." });
+    }
+    if (content.length > 2000) {
+      return res.status(400).json({ error: "Letter is too long." });
+    }
 
     //create a new Letter object
     const letter = await Letter.create({
-        senderID,
-        recipientID,
+        sender_id: senderID,
+        recipient_id: recipientID,
         content
     });
 
     res.status(201).json(letter);
 
-  } catch {
+  } catch (error) {
     console.error(error);
     res.status(400).json({ error: error.message });
   }
