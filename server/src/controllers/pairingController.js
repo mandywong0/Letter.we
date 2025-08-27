dae88f4a92b1fdea8464de8c4132a183472c6dbd
mandywong00 <mandywong@berkeley.edu>
2025-08-27 16:18:03 -0700

    finished generate code method

--- server/src/controllers/pairingController.js
+++ server/src/controllers/pairingController.js
@@ -1,16 +1,27 @@
 const crypto = require("crypto"); 
 
 const generateCode = async (req, res) => {
-  try {
-    //generate code
-    const code = crypto.randomInt(100000, 1000000).toString();
-    //store code
-      //make sure code doesn't already exist in db
-    //send code
+  let code;
+  let newCode;
+  let success = false;
 
-  } catch (error) {
-
+  for (let tries = 0; tries < 5 && !success; tries++) {
+    code = crypto.randomInt(100000, 1000000).toString();
+    try {
+      newCode = await PairingCode.create({
+        code,
+        user_id: req.user.id,
+        expires_at: new Date(Date.now() + 15 * 60 * 1000),
+      });
+      success = true;
+    } catch (error) {
+      console.error(error);
+    }
   }
+  if (!success) {
+    return res.status(500).json({ error: "Failed to generate code" });
+  }
+  return res.status(201).json({ code, expires_at });
 };
 
 module.exports = { generateCode };
